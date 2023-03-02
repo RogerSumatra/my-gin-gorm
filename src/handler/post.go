19bc0c35faabf504bd8674fcfceebd3452624fb8
Daffarel <farelalden96@gmail.com>
2023-03-02 12:16:50 +0700

    add endpoint get post by id

--- src/handler/post.go
+++ src/handler/post.go
@@ -25,6 +25,72 @@ func (h *handler) createPost(ctx *gin.Context) {
 		return
 	}
 
-	h.SuccessResponse(ctx, http.StatusOK, "Successfully created new post", nil)
+	h.SuccessResponse(ctx, http.StatusOK, "Successfully created new post", nil, nil)
+
+}
+
+func (h *handler) getListPost(ctx *gin.Context) {
+	var postParam entity.PostParam
+
+	if err := h.BindParam(ctx, &postParam); err != nil {
+		h.ErrorResponse(ctx, http.StatusBadRequest, "invalid request body", nil)
+		return
+	}
+
+	postParam.FormatPagination()
+
+	var posts []entity.Post
+
+	if err := h.db.
+		Model(entity.Post{}).
+		Limit(int(postParam.Limit)).
+		Offset(int(postParam.Offset)).
+		Find(&posts).Error; err != nil {
+		h.ErrorResponse(ctx, http.StatusInternalServerError, err.Error(), nil)
+		return
+
+	}
+
+	var totalElements int64
+
+	if err := h.db.
+		Model(entity.Post{}).
+		Limit(int(postParam.Limit)).
+		Offset(int(postParam.Offset)).
+		Count(&totalElements).Error; err != nil {
+		h.ErrorResponse(ctx, http.StatusInternalServerError, err.Error(), nil)
+		return
+	}
+
+	postParam.ProcessPagination(totalElements)
+
+	h.SuccessResponse(ctx, http.StatusOK, "Successfully get list posts", posts, &postParam.PaginationParam)
+}
+
+func (h *handler) getPost(ctx *gin.Context) {
+	var postParam entity.PostParam
+
+	if err := h.BindParam(ctx, &postParam); err != nil {
+		h.ErrorResponse(ctx, http.StatusBadRequest, "bad param", nil)
+		return
+	}
+
+	// var PostPayLoad entity.PostBody
+	// if err := h.BindBody(ctx, &PostPayLoad); err != nil {
+	// 	h.ErrorResponse(ctx, http.StatusBadRequest, "bad request", nil)
+	// 	return
+	// }
+
+	var post entity.Post
+	if err := h.db.Model(&post).Where(&postParam).Error; err != nil {
+		h.ErrorResponse(ctx, http.StatusInternalServerError, "couldn't get post", nil)
+		return
+	}
+
+	h.SuccessResponse(ctx, http.StatusOK, "post found", post, nil)
+
+}
+
+func (h *handler) updatePost(ctx *gin.Context) {
 
 }
